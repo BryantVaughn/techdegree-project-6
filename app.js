9bd48d80003f349de5f96d5e892c00ce260dfd3b
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-11-02 21:13:28 -0700

    Add comments to created functions

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const keyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const lives = document.querySelectorAll('.tries');
 
-// Initialize variables
+// Declare/Initialize variables
 let missed;
 const phrases = [
 	'Accidentally on purpose',
@@ -36,10 +36,15 @@ keyboard.addEventListener('click', (evt) => {
 		if (!letterFound) updateLives();
 		setTimeout(() => {
 			checkWin();
-		}, 500);
+		}, 700);
 	}
 });
 
+/*
+ * initializeGame function
+ * Prepares all elements on the game for a
+ * new round to be played.
+ */
 function initializeGame() {
 	missed = 0;
 	initializePhrase();
@@ -47,12 +52,20 @@ function initializeGame() {
 	initializeLives();
 }
 
+/*
+ * initializePhrase function
+ * Initializes the phrase element for a new round.
+ */
 function initializePhrase() {
 	while (phrase.firstElementChild) {
 		phrase.removeChild(phrase.firstElementChild);
 	}
 }
 
+/*
+ * initializeKeyboard function
+ * Initializes the keyboard for new round.
+ */
 function initializeKeyboard() {
 	const keyrows = keyboard.children;
 	for (let keyrow of keyrows) {
@@ -64,12 +77,21 @@ function initializeKeyboard() {
 	}
 }
 
+/*
+ * initializeLives function
+ * Initializes the life images for a new round.
+ */
 function initializeLives() {
 	for (let life of lives) {
 		life.firstElementChild.src = 'images/liveHeart.png';
 	}
 }
 
+/*
+ * checkWin function
+ * Checks to see if all lives have been used, or if
+ * the phrase has been solved.
+ */
 function checkWin() {
 	if (missed >= 5) displayOverlay('lose');
 	else {
@@ -80,12 +102,22 @@ function checkWin() {
 	}
 }
 
+/*
+ * displayOverlay function
+ * Takes a classname as an input then displays the correct
+ * overlay by applying the classname.
+ */
 function displayOverlay(className) {
 	const overlay = startBtn.parentNode;
 	overlay.classList.add(className);
 	overlay.style.display = '';
 }
 
+/*
+ * updateLives function
+ * Updates the liveHeart image to the lostHeart image
+ * when there's a wrong guess.
+ */
 function updateLives() {
 	const lifeImage = lives[missed].firstElementChild;
 	lifeImage.src = 'images/lostHeart.png';
@@ -103,12 +135,21 @@ function getRandomPhraseAsArray(arr) {
 	return characters;
 }
 
+/*
+ * addPhraseToDisplay function
+ * Takes the characterArr as input and then calls the append
+ * method with each individual character.
+ */
 function addPhraseToDisplay(characterArr) {
 	for (let character of characterArr) {
 		appendCharacterLi(character);
 	}
 }
 
+/*
+ * checkLetter function
+ * Checks to see if guessed letter is in the phrase or not.
+ */
 function checkLetter(selectedLetter) {
 	const letters = document.querySelectorAll('.letter');
 	let matchingLetter = null;
@@ -121,11 +162,22 @@ function checkLetter(selectedLetter) {
 	return matchingLetter;
 }
 
+/*
+ * appendCharacterLi function
+ * Takes a character in as a parameter and calls the li
+ * creation method, then appends the li to the phrase element.
+ */
 function appendCharacterLi(character) {
 	const li = createCharacterLi(character);
 	phrase.appendChild(li);
 }
 
+/*
+ * createCharacterLi function
+ * Takes a character as input and builds a li element with
+ * the character as textContent. It applies the correct class
+ * to the li based on if the character is a letter or space.
+ */
 function createCharacterLi(character) {
 	const li = document.createElement('li');
 	li.textContent = character;
